5b0ed2dd92b3302558b15dbee885cf2331fcc431
Douglas Berkley <douglasmberkley@gmail.com>
2024-07-22 16:39:22 +0900

    style of bt

--- scripts/detect.js
+++ scripts/detect.js
@@ -2,5 +2,5 @@ const neverGonnaGiveYouUp = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 const links = document.querySelectorAll(`[href='${neverGonnaGiveYouUp}']`);
 links.forEach((link) => {
   link.innerText = "🚨 " + link.innerText + " 🚨"
-  link.insertAdjacentHTML('afterend', "<p style='color: red;font-weight: bolder;padding: 8px;'>RICK-ROLL ALERT</p>")
+  link.insertAdjacentHTML('afterend', "<p style='color: red;font-weight: bolder;'>☝️ RICK-ROLL ALERT ☝️</p>")
 })
